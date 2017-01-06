61fff89348b1e27ba50f35cd12778d6f176a5850
Uli Guenter <ipv6@clarisweb.de>
2017-01-06 17:52:27 +0100

    added form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -17,5 +17,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
  console.log('newMessage', message);
+ var li = jQuery('<li></li>');
+ li.text(`${message.from}: ${message.text}`)
 
+ jQuery('#messages').append(li);
+});
+
+
+// socket.emit('createMessage', {
+//   from: 'Uli',
+//   text: 'All right now'
+// }, function (data) {
+//   console.log('here we go', data);
+// });
+
+ jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+   from: 'User',
+   text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
